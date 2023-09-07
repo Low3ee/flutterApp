b42b78a16710c4105243b10b11b456425f2c430e
Low3ee <louieabad000@gmail.com>
2023-09-08 07:30:44 +0800

    Log in Page

--- lib/Pages/home.page.dart
+++ lib/Pages/home.page.dart
@@ -2,7 +2,6 @@ import 'dart:html';
 
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -10,22 +9,84 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: const Text("hello world", 
+        title: const Text(
+          "App nako",
           style: TextStyle(
-              color: Colors.black,
-              fontSize: 18,
-              fontWeight: FontWeight.bold
-            ),
-              ),
-            backgroundColor: Colors.white12,
-            centerTitle: true,
-          ),
-      body: 
-         
-           const Center(
-              child: Text("Hello World"),
-            )
-    
+              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.white12,
+        centerTitle: true,
+      ),
+      body: const MyForm(),
     );
+  }
+}
+
+class MyForm extends StatefulWidget {
+  const MyForm({super.key});
+
+  @override
+  MyFormState createState() {
+    return MyFormState();
+  }
+}
+
+class MyFormState extends State<MyForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: FractionallySizedBox(
+            widthFactor: 0.8,
+            heightFactor: 0.9,
+            child:  Form(
+                    key: _formKey,
+                    child: (Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        TextFormField(
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Enter Your Name',
+                              labelText: 'Name'),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Please enter Name";
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox( height: 15),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Enter Your Password',
+                              labelText: 'Password'),
+                          obscureText: true,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Please enter Password";
+                            }
+                            return null;
+                          },
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                print()
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text('Processing Data')));
+                              }
+                              ;
+                            },
+                            child: const Text("Submit"),
+                          ),
+                        )
+                      ],
+                    )))));
   }
 }
