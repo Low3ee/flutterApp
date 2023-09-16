79c7bc2dc6017fd3937486903dcf88a8a7fe4322
Low3ee <louieabad000@gmail.com>
2023-09-17 04:16:45 +0800

    try styles

--- lib/Pages/home.page.dart
+++ lib/Pages/home.page.dart
@@ -16,56 +16,73 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Center(child: Text('HOME'))),
-      body: ListView(
-        children: [
-          InkWell(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const ContainerPage()));
-            },
-            child: const Card(
-              child: Padding(
-                  padding: EdgeInsets.all(12), child: Text("1.) Container")),
-            ),
-          ),
-          InkWell(
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const ColumnPage()));
-            },
-            child: const Card(
-              child: Padding(
-                  padding: EdgeInsets.all(12), child: Text("2.) Column")),
-            ),
-          ),
-          InkWell(
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const RowPage()));
-            },
-            child: const Card(
-              child: Padding(
-                padding: EdgeInsets.all(12),
-                child: Text("3.) Row"),
+      backgroundColor: Color.fromARGB(136, 51, 4, 221),
+      appBar: AppBar(
+        title: const Center(
+            child: Text(
+                style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontWeight: FontWeight.bold),
+                'HOME')),
+        backgroundColor: Color.fromARGB(125, 66, 25, 179),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('Assets/maxresdefault.jpg'),
+                fit: BoxFit.cover)),
+        child: ListView(
+          children: [
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ContainerPage()));
+              },
+              child: const Card(
+                child: Padding(
+                    padding: EdgeInsets.all(12), child: Text("1.) Container")),
               ),
             ),
-          ),
-          InkWell(
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const TextPage()));
-            },
-            child: const Card(
-              child: Padding(
-                padding: EdgeInsets.all(12),
-                child: Text("4.) Text Page"),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ColumnPage()));
+              },
+              child: const Card(
+                child: Padding(
+                    padding: EdgeInsets.all(12), child: Text("2.) Column")),
               ),
             ),
-          )
-        ],
+            InkWell(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const RowPage()));
+              },
+              child: const Card(
+                child: Padding(
+                  padding: EdgeInsets.all(12),
+                  child: Text("3.) Row"),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const TextPage()));
+              },
+              child: const Card(
+                child: Padding(
+                  padding: EdgeInsets.all(12),
+                  child: Text("4.) Text Page"),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
